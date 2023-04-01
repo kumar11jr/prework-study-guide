6d0c1b106b551571dc585e32fa5ae772537a8e70
Prabhat Kumar <104729493+kumar11jr@users.noreply.github.com>
2023-04-01 20:13:07 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -25,3 +25,39 @@ console.log("Here are the topics that we learned in prework:");
 listTopics();
 console.log("Which topic should we study first");
 selectTopic();
+
+var header = document.getElementById("top");
+var card = document.getElementById("card");
+var foot = document.getElementById("foot");
+var display = 0;
+const Dark = () =>{
+  if (display==0) {
+    ModeD();
+  }else{
+   ModeL();
+  }
+  
+}
+
+const ModeD = () =>{
+  document.body.style.backgroundColor = "black";
+  header.style.backgroundColor="black";
+  header.style.color = "white";
+  card.style.color = "white";
+  foot.style.backgroundColor = "black";
+  foot.style.color = "white";
+  ++display;
+ 
+}
+
+const ModeL= ()=>{
+  document.body.style.backgroundColor = "white";
+  header.style.backgroundColor="rgb(86, 86, 184)";
+  header.style.color = "white";
+  card.style.color = "black";
+  foot.style.backgroundColor = "rgb(86, 86, 184)";
+  foot.style.color = "white";
+  --display;
+  
+}
+
